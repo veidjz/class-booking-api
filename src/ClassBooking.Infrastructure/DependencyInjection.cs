a3b4fbd8347fa2f14ab5ffb32149383bfc1116db
veidjz <veidjz@proton.me>
2026-07-19 17:19:42 -0300

    feat(infrastructure): bump the aggregate version token on save

--- src/ClassBooking.Infrastructure/DependencyInjection.cs
+++ src/ClassBooking.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using ClassBooking.Application.Abstractions.Data;
 using ClassBooking.Infrastructure.Persistence;
+using ClassBooking.Infrastructure.Persistence.Interceptors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,13 +14,16 @@ public static class DependencyInjection
     string connectionString = configuration.GetConnectionString("Database")
         ?? throw new InvalidOperationException("The connection string 'Database' is not configured.");
 
-    services.AddDbContextPool<AppDbContext>(options =>
+    services.AddSingleton<VersionTokenInterceptor>();
+
+    services.AddDbContextPool<AppDbContext>((serviceProvider, options) =>
         options
             .UseMySql(
                 connectionString,
                 new MySqlServerVersion(new Version(8, 4, 0)),
                 mySql => mySql.MigrationsAssembly("ClassBooking.Infrastructure"))
-            .UseSnakeCaseNamingConvention());
+            .UseSnakeCaseNamingConvention()
+            .AddInterceptors(serviceProvider.GetRequiredService<VersionTokenInterceptor>()));
 
     services.AddScoped<IAppDbContext>(serviceProvider => serviceProvider.GetRequiredService<AppDbContext>());
 
